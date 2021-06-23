9dbc646d9b602a49fa627bb504c99c75041949f8
StephenWalter <stephen_walter@hotmail.com>
2021-06-23 15:18:33 +0100

    Initial commit

---
+++ oauth2_accesstoken_test.py
@@ -0,0 +1,23 @@
+from linkedin import linkedin
+
+# CLIENT_ID = '<Your Client ID>'
+# CLIENT_SECRET = '<Your Client secret>'
+
+
+RETURN_URL = 'http://localhost:8080/code/'
+
+authentication = linkedin.LinkedInAuthentication(
+                    CLIENT_ID,
+                    CLIENT_SECRET,
+                    RETURN_URL,
+                    linkedin.PERMISSIONS.enums.values()
+                )
+
+# Note: edit permissions according to what you defined in the linkedin
+# developer console.
+
+authentication.authorization_code = '#########################################'
+result = authentication.get_access_token()
+
+print("Access Token:", result.access_token)
+print("Expires in (seconds):", result.expires_in)
